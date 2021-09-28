9dcce45fff428073147e96699a9fdc88b8a95822
System Administrator <root@Joesphs-Mac-mini.local>
2021-09-28 10:31:31 -0500

    added django_app

---
+++ django_app/static_django/js/scrollreveal.js
@@ -0,0 +1,35 @@
+window.sr = ScrollReveal();
+sr.reveal('.home-img',{
+    duration: 1500,
+    origin: "top",
+    distance: "300px"
+});
+sr.reveal('.home-text',{
+    duration: 2000,
+    origin: "left",
+    distance: "300px"
+});
+sr.reveal('.btn-work',{
+    duration: 2000,
+    origin: "left",
+    delay: 200,
+    distance: "100px"
+});
+sr.reveal('.student-card',{
+    duration: 1500,
+    origin: "left",
+    distance: "100px",
+    viewFactor: 0.2
+});
+sr.reveal('.dev-card',{
+    duration: 1500,
+    origin: "bottom",
+    distance: "100px",
+    viewFactor: 0.2
+});
+sr.reveal('.music-card',{
+    duration: 1500,
+    origin: "right",
+    distance: "100px",
+    viewFactor: 0.2
+});
